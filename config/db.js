62b566799eb39b2c348a3805d474dbc4fe778f2e
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-10-25 22:17:32 +0200

    test connection and try the first query

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
+// require("dotenv").config(); // added just to run this script alone
+
 // setting up the connection with the MySql database
 // 'mysql2' provides a promise wrapper to work with the database unlike 'mysql'
 const mysql = require("mysql2");
@@ -6,8 +8,20 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    passwod: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 });
+
+/* - testing the connection - */
+// let query = "SELECT * FROM users";
+
+// pool.query(query, (err, res) => {
+//     if (err) throw err;
+
+//     console.log(res);
+// })
+
+// works just fine #
+/* -------------------------- */
 
 // to have access to the database in other files
 module.exports = pool.promise();
